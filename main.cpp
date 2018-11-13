9f59e66cb0df935e5d70df90cfc4867c88326a06
xin0111 <xinnew0111@sina.cn>
2018-11-13 10:06:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	CopyFilesWindow w;
+	if (argc == 2)
+	{
+		w.importFromXml(QString::fromLocal8Bit(argv[1]));
+	}
 	w.show();
 	return a.exec();
 }
